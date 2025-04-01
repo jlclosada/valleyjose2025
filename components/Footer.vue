cb3d6ccc7d0ca8e1aeda73a83113aa208f992952
Jose Cáceres Losada <jose-luis.caceres@basf.com>
2025-04-01 16:05:44 +0200

    Changed padding in footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <!-- components/Footer.vue -->
 <template>
   <footer class="backdrop-blur-lg bg-white/60 shadow-md text-blue-900 py-6 text-center mt-10">
-    <p class="text-lg">&copy; Valle y José Luis 2025. Todos los derechos reservados.</p>
+    <p class="text-lg p-4">&copy; Valle y José Luis 2025. Todos los derechos reservados.</p>
   </footer>
 </template>
